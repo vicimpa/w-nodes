27c390583e8bd7dc936db0e7ba3638015a4819d1
PromiSe#### <vicimpa@ya.ru>
2024-08-28 22:32:47 +0700

    Last fix

--- src/components/audio-context/index.tsx
+++ src/components/audio-context/index.tsx
@@ -1,17 +1,10 @@
 import "./_nodes";
 
-import { Destination } from "./nodes/Destination";
-import { Gain } from "./nodes/effects/Gain";
-
 export const AudioContext = () => {
 
   return (
     <>
-      <Destination />
-      <Gain />
-      <Gain />
-      <Gain />
-      <Gain />
+
     </>
   );
 };
