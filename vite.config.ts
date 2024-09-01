efab30d266e83a48cab90951807600b623bc259b
PromiSe#### <vicimpa@ya.ru>
2024-09-01 21:10:44 +0700

    Fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react-swc';
 export default defineConfig({
   root: './src',
   publicDir: '../public',
+  base: './',
   build: {
     emptyOutDir: true,
     outDir: '../dist',
