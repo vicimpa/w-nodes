c06130b8a6ea17f4a26227c470b578051c711d18
PromiSe#### <vicimpa@ya.ru>
2024-09-05 21:38:35 +0700

    Fix schema

--- src/components/node-editor/node-project/plugins/detectCopy.ts
+++ src/components/node-editor/node-project/plugins/detectCopy.ts
@@ -13,8 +13,8 @@ const copyPack = makePack(
     nodes: t.array(t.uint()),
     connect: t.array(
       t.tuple(
-        t.tuple(t.int(), t.int()),
-        t.tuple(t.int(), t.int()),
+        t.tuple(t.uint(), t.uint()),
+        t.tuple(t.uint(), t.uint()),
       )
     ),
     configs: t.array(t.map())
