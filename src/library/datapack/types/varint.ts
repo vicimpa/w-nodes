3941a4ae38641f843f11afbb646b59eecd628ca4
PromiSe#### <vicimpa@ya.ru>
2024-09-08 11:09:59 +0700

    Append fix varint equal

--- src/library/datapack/types/varint.ts
+++ src/library/datapack/types/varint.ts
@@ -48,7 +48,7 @@ export default cached(() => {
       return store.data[store.cursor++];
     },
     equal(value) {
-      return typeof value === "number" && (value | 0) === value;
+      return typeof value === "number" && value >= 0 && (value | 0) === value;
     }
   });
 });
