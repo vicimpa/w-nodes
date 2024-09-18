d843ac5f66178c59dafab006d9509890c13e291d
PromiSe#### <vicimpa@ya.ru>
2024-09-19 01:21:27 +0700

    REMOVE TRASH

--- src/components/audio-context/index.tsx
+++ src/components/audio-context/index.tsx
@@ -1,7 +1,6 @@
 import { AudioProject } from "./AudioProject";
 import { Debug } from "./views/debug/Debug";
 import { Menu } from "./views/menu";
-import drums from "./assets/drums";
 
 export const AudioContext = () => (
   <AudioProject>
