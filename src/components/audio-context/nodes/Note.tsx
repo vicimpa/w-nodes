abdad6aa34d34ade5f020a8f42d6ac09c0931490
PromiSe#### <vicimpa@ya.ru>
2025-10-16 00:42:17 +0300

    Small fixes

--- src/components/audio-context/nodes/Note.tsx
+++ src/components/audio-context/nodes/Note.tsx
@@ -4,37 +4,23 @@ import { group } from "../_groups";
 import { name } from "$library/function";
 import { store } from "$components/node-editor";
 import { signalRef } from "$library/signals";
-import { effect } from "@preact/signals-react";
-import { resizeObserver } from "@vicimpa/observers";
-import { reactive } from "@vicimpa/decorators";
+import { dom } from "$library/dom";
 
 @name('Note')
 @group('high')
-@reactive()
 export default class extends BaseNode {
-  @store sizeWidth = 100;
-  @store sizeHeight = 100;
-  @store contentText = '';
+  area = dom('textarea', { onkeydown: e => (!e.ctrlKey && !e.metaKey) && e.stopPropagation() });
+
+  @store get sizeWidth() { return this.area.offsetWidth; };
+  set sizeWidth(v) { this.area.style.width = v + 'px'; }
+  @store get sizeHeight() { return this.area.offsetHeight; };
+  set sizeHeight(v) { this.area.style.height = v + 'px'; }
+  @store get contentText() { return this.area.value; };
+  set contentText(v) { this.area.value = v; }
 
   ref = signalRef<HTMLTextAreaElement>();
 
-  _connect = () => (
-    effect(() => {
-      const { value: now } = this.ref;
-      if (!now) return;
-      now.style.width = this.sizeWidth + 'px';
-      now.style.height = this.sizeHeight + 'px';
-      now.value = this.contentText;
-      return resizeObserver(now, () => {
-        this.sizeWidth = now.offsetWidth;
-        this.sizeHeight = now.offsetHeight;
-      });
-    })
-  );
-
   _view = () => (
-    <textarea
-      ref={this.ref}
-      onInput={({ currentTarget }) => this.contentText = currentTarget.value} />
+    <div style={{ display: 'contents' }} ref={r => { r?.appendChild(this.area); }} />
   );
 };
